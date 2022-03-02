21001fa830643682c821171bd0508743b947b49f
MarceloPN <marcelonogueira072@gmail.com>
2022-03-01 22:49:31 -0300

    Login completo, criados os pacotes:DTO e repository, Implementados os dateils

--- src/main/java/com/devsuperior/movieflix/MovieflixApplication.java
+++ src/main/java/com/devsuperior/movieflix/MovieflixApplication.java
@@ -6,8 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieflixApplication {
 
+/*@Autowired
+	private  UserRepository userRepository;
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(MovieflixApplication.class, args);
 	}
+	
+	
+	/*@Override
+	public void run(String...args)throws Exception{
+	
+		List<User> list = userRepository.findByEmail("RS");
+	}	*/	
+
 
 }
--- src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.devsuperior.movieflix.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.movieflix.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	User findByEmail(String findByEmail);
+	
 }
